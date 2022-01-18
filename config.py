568114d13d04f02aa9eb3655150bab0b120243c1
erpsycho18 <97943504+erpsycho18@users.noreply.github.com>
2022-01-18 14:23:03 +0530

    psychoxiris commit

---
+++ config.py
@@ -0,0 +1,35 @@
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = ""
+    OWNER_ID = "❰-𝘾𝙁𝘾-❱ॐ⫸𒆜໓૨.℘𝔰ყƈɧσ™𒆜⫷ॐ⚜️τнєτιταиѕνєяιƒιє∂🇮🇳⚜️"
+    OWNER_USERNAME = "dr_psycho_15"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = True
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    ALLOW_EXCL = False  # Allow ! commands as well as /
+
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
