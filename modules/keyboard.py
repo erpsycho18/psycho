cb2b92b8f784d6f13a9e7cc6c405f5710a30c7f9
erpsycho18 <97943504+erpsycho18@users.noreply.github.com>
2022-01-18 22:47:17 +0530

    Update keyboard.py

--- modules/keyboard.py
+++ modules/keyboard.py
@@ -4,11 +4,11 @@ from typing import List, Dict
 from telegram import Bot, ParseMode, ReplyKeyboardMarkup, KeyboardButton
 from telegram.error import TelegramError
 
-from tg_bot import dispatcher
-#from tg_bot.modules.translations.strings import tld
+from tgbot import dispatcher
+#from tgbot.modules.translations.strings import tld
 from telegram.ext import CommandHandler, Filters, MessageHandler, CallbackQueryHandler
 
-import tg_bot.modules.sql.connection_sql as con_sql
+import tgbot.modules.sql.connection_sql as con_sql
 
 def keyboard(bot, update):
     user = update.effective_user  # type: Optional[User]
