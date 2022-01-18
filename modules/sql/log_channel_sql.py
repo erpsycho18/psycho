aa7e39edb920054ec50c86d0bdd3ab5c1a816686
erpsycho18 <97943504+erpsycho18@users.noreply.github.com>
2022-01-18 23:31:51 +0530

    Update log_channel_sql.py

--- modules/sql/log_channel_sql.py
+++ modules/sql/log_channel_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, func, distinct
 
-from tg_bot.modules.sql import BASE, SESSION
+from tgbot.modules.sql import BASE, SESSION
 
 
 class GroupLogs(BASE):
