3c2d7dbdfeb1b58a61d06b4c1717665f8ec19afe
erpsycho18 <97943504+erpsycho18@users.noreply.github.com>
2022-01-18 22:37:25 +0530

    Update antiflood.py

--- modules/antiflood.py
+++ modules/antiflood.py
@@ -6,10 +6,10 @@ from telegram.error import BadRequest
 from telegram.ext import Filters, MessageHandler, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
-from tg_bot import dispatcher
-from tg_bot.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict
-from tg_bot.modules.log_channel import loggable
-from tg_bot.modules.sql import antiflood_sql as sql
+from tgbot import dispatcher
+from tgbot.modules.helper_funcs.chat_status import is_user_admin, user_admin, can_restrict
+from tgbot.modules.log_channel import loggable
+from tgbot.modules.sql import antiflood_sql as sql
 
 FLOOD_GROUP = 3
 
